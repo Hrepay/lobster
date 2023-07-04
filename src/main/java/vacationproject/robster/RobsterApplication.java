a1fc230f627fc35df5294c06b82f7320bd0cc602
Hwang Sang Hwan <dbrdldh11@naver.com>
2023-07-04 16:30:14 +0900

    test

---
+++ src/main/java/vacationproject/robster/RobsterApplication.java
@@ -0,0 +1,13 @@
+package vacationproject.robster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vacationproject/robster/RobsterApplicationTests.java
@@ -0,0 +1,13 @@
+package vacationproject.robster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
